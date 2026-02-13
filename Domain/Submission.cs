ec145277933ed553a2361d0c1b5aa606944c8678
pashkov-dmitriy <pashkov.dmitriy2000@gmail.com>
2026-02-13 17:35:44 +0300

    Domain entities added

---
+++ Domain/Submission.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Submission
+    {
+        public Guid ApplicationId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public SubmissionCreator Author { get; set; }
+        public Reviewer? Reviewer { get; set; }
+        public SubmissionStatus Status { get; set; }
+
+        public Submission(string title, string description, SubmissionCreator author, SubmissionStatus status) 
+        {
+            Title = title;
+            Description = description;
+            Author = author;
+            Status = status;
+        }
+    }
+}
---
+++ Domain/SubmissionCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class SubmissionCreator : User
+    {
+        public Submission CreateSubmission(string title, string description)
+        {
+            return new Submission(title, description, this, SubmissionStatus.Project); 
+        }
+    }
+}
